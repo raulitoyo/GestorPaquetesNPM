89539699e328644e89827cd927828b2707e48fe6
Raul Vargas <raulito@gmail.com>
2018-12-12 20:41:56 -0400

    Primer release

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.end("<button type='button'>Abrir</button>");
-
+    //response.writeHead(200, {"Content-Type": "text/html"});
+    //response.end("<button type='button' class='btn btn-primary btn-lg'>Abrir</button>");
+    fs.readFile('inicio.html',function (err, data){
+        response.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        response.write(data);
+        response.end();
+    });
 });
 
 var port = 8081;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+
